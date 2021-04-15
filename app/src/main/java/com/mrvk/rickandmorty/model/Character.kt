417dd4169009da4166503433e0ede9b7a8b66e45
mervekas <mervekkas@icloud.com>
2021-04-15 13:50:18 +0300

    Libraries Setted Models Created Views Created Navigation Cretaed

---
+++ app/src/main/java/com/mrvk/rickandmorty/model/Character.kt
@@ -0,0 +1,12 @@
+package com.mrvk.rickandmorty.model
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+data class Character(
+    @SerializedName("info") @Expose
+    var info: Info?,
+    @SerializedName("results")
+    @Expose
+    var results: MutableList<Result>?) {
+}
--- app/src/main/java/com/mrvk/rickandmorty/view/MainActivity.kt
+++ app/src/main/java/com/mrvk/rickandmorty/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.mrvk.rickandmorty
+package com.mrvk.rickandmorty.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mrvk.rickandmorty.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
